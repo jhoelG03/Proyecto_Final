1cac91ff70dca701c6923ea7d79ae59c4c979346
jhoelG03 <jhoel.guaman9337@utc.edu.ec>
2025-02-10 15:51:18 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById('modoOscuro').addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
+});
+
+document.querySelectorAll("nav ul li a").forEach(link => {
+    link.addEventListener("click", function() {
+        document.querySelectorAll("nav ul li a").forEach(el => el.classList.remove("active"));
+        this.classList.add("active");
+    });
+
+});
+
